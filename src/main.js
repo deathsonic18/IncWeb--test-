2f4a2e6897f986daad060ced23a14132129eeb8e
deathsonic18 <georgescerrimontaldo@hotmail.com>
2017-03-09 14:45:42 +0100

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -7,17 +7,14 @@
 */
   function startPonsify() {
 
-//**********************
-    function ponsify(x, y, z)
+    function ponsify(x, y, z,getTestNumber)                                     //lower,limit,steps,testnumber
     {
-        // Add your code here
-        if(x == y) {console.log("this is x == y : "+x );return x;};           //if lower = limit{return x}
-
-        console.log("Hello world!");
+        console.log("This is test number : " +getTestNumber);
+        if(x == y) {console.log("this is x == y : "+x );return null;};           //if lower = limit{return x}
         console.log(x,y,z);                                                     //displays lower, limit & step figures
 
         // if(x < 0) {console.log("this is i is negative "+x);return 0};        //if lower is negative{break}
-        if(y < x) {{console.log("this is invaild y < x "+x );return 0;};}       //if higher < lower {return 0;}
+        if(y < x) {{console.log("this is invaild y < x "+x );return null;};}       //if higher < lower {return 0;}
         for (i = x+1; i < y; i++)                                               //incrementation;
         {
           i = i+z-1;
@@ -25,18 +22,14 @@
           console.log(i);                                                       //displys sum
           if(y < i) {console.log("this is y < x : "+i );return 0;};             //if upper < lower{return 0}
           var sum = (i * (i+1)) / 2-1;                                          //calc's sum
-          sumReturn(sum);                                                       //sends sum to sumReturn function
           getReturn(i,y,z);                                                     //sends values to getReturn function
-          // console.log(sum);
-
         }
+        sumReturn(sum);                                                       //sends sum to sumReturn function
+
+        console.log("-----");
 
     }
-    ponsify(2,6000,7);                                                       //send parameters to function (lower,limit,step);
 
-
-
-//**********************
     // required to export for tests
     // DO NOT TOUCH
     var exports = module.exports;
@@ -45,11 +38,16 @@
 
     function getReturn(getX,getY,getZ)                                          //gets returns for every epoch
     {
+      // console.log("returnX : " +getX );
+      // console.log("returnX : " +getY );
+      // console.log("returnX : " +getZ );
+
       return getX,getY,getZ;
     }
 
     function sumReturn(sum)                                                     //gets sum
     {
+      console.log("sum : " +sum );
       return sum;
     }
 
