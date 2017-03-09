77e13496388a2af7473d54f202ccb64681f0cb09
deathsonic18 <georgescerrimontaldo@hotmail.com>
2017-03-09 16:43:51 +0100

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -9,24 +9,22 @@
 
     function ponsify(x, y, z,getTestNumber)                                     //lower,limit,steps,testnumber
     {
-        console.log("This is test number : " +getTestNumber);
-        if(x == y) {console.log("this is x == y : "+x );return null;};           //if lower = limit{return x}
-        console.log(x,y,z);                                                     //displays lower, limit & step figures
+      console.log("This is test number : " +getTestNumber);
 
-        // if(x < 0) {console.log("this is i is negative "+x);return 0};        //if lower is negative{break}
-        if(y < x) {{console.log("this is invaild y < x "+x );return null;};}       //if higher < lower {return 0;}
-        for (i = x+1; i < y; i++)                                               //incrementation;
-        {
-          i = i+z-1;
-          if(i > y) {console.log("this is x > y "+i);return x; break;};         //if lower > limt {return i}
-          console.log(i);                                                       //displys sum
-          if(y < i) {console.log("this is y < x : "+i );return 0;};             //if upper < lower{return 0}
-          var sum = (i * (i+1)) / 2-1;                                          //calc's sum
-          getReturn(i,y,z);                                                     //sends values to getReturn function
-        }
-        sumReturn(sum);                                                       //sends sum to sumReturn function
+      if(x==y) {console.log("this is x == y : "+x );return null;}
+      else if(y < x) {{console.log("this is invaild y < x "+x );return null;};}
 
-        console.log("-----");
+      for (i = x+1; i < y; i++)                                               //incrementation;
+      {
+        i = i+z-1;
+        if(i > y) {console.log("this is x > y "+i);return x; break;};         //if lower > limt {return i}
+        console.log(i);                                                       //displys sum
+        if(y < i) {console.log("this is y < x : "+i );return 0;};             //if upper < lower{return 0}
+        var sum = (i * (i+1)) / 2-1;                                          //calc's sum
+        getReturn(i,y,z);                                                     //sends values to getReturn function
+      }
+      sumReturn(sum);
+      console.log("-----");
 
     }
 
